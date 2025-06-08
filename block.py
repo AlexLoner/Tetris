bc70cd22e81b89546bef530def99b4f9ee069879
AlexLoner <lukyanov9567@gmail.com>
2025-06-08 21:13:23 +0300

    add tracking other figure boarders

--- block.py
+++ block.py
@@ -34,8 +34,11 @@ class Block:
         bottom_border = self.y + shift_y + sts.cell_size <= rect.bottom
         return bottom_border
     
-    # def check_collisions(self, shift_x, shift_y, inactive_blocks):
-    #     pygame.sprite.collide_rect(self, inactive_blocks)
+    def check_collisions(self, shift_x, shift_y, inactive_blocks):
+        self.move(shift_x, shift_y)
+        collision = pygame.sprite.spritecollide(self, inactive_blocks, False)
+        self.move(-shift_x, -shift_y)
+        return collision
 
 class Figure:
 
@@ -62,6 +65,10 @@ class Figure:
                         color=self.color
                     )
                     self.blocks.append(single_block)
+    
+    def check_collisions(self, shift_x, shift_y, inactive_blocks):
+        # Отрицание потому что метод проверяет наличие столкновения при заданном смещении блока фигуры  
+        return all([not block.check_collisions(shift_x, shift_y, inactive_blocks) for block in self.blocks])
 
     def check_sides(self, surface, shift):
         return all([block.check_sides(surface, shift) for block in self.blocks])
@@ -69,7 +76,7 @@ class Figure:
     def check_bottom(self, surface, shift):
         return all([block.check_bottom(surface, shift) for block in self.blocks])
     
-    def move(self, surface):
+    def move(self, surface, inactive_blocks):
         if not self.active:
             return
         
@@ -81,25 +88,60 @@ class Figure:
             shift_x = sts.step_side
         if self.moving_down:    
             shift_y = shift_y * sts.speed_coef
-        
-        coef_x = self.check_sides(surface, shift_x)
-        coef_y = self.check_bottom(surface, shift_y)
-        
-        # Спускаемся на недостающий участок, чтобы фигура всегда прижималась к низу 
-        if coef_y == 0:
-            shift_y /= sts.speed_coef
-            coef_y = self.check_bottom(surface, shift_y)
 
+        # (Проверка столкновений с другими блоками) and (Проверка границ игрового поля)
+        coef_x = self.check_collisions(shift_x, 0, inactive_blocks) and self.check_sides(surface, shift_x)
+        coef_y = self.check_collisions(0, shift_y, inactive_blocks) and self.check_bottom(surface, shift_y)
+    
+        if coef_y == 0:
+            while (self.check_collisions(0, sts.bottom_correction, inactive_blocks) 
+                   and self.check_bottom(surface, sts.bottom_correction)
+            ):
+                for block in self.blocks:
+                    block.move(0, sts.bottom_correction)
+            self.active = False
+            return 
+        
         for block in self.blocks:
             block.move(coef_x * shift_x, coef_y * shift_y)
+        
         self.x += coef_x * shift_x
         self.y += coef_y * shift_y
-        if coef_y == 0:
-            self.active = False
+            
 
-    def rotate(self):
+    def rotate(self, surface, inactive_blocks):
+        if not self.active:
+            return
+        
+        # Сохраняем текущее состояние
+        old_type = self.type.copy()
+        old_blocks = [block for block in self.blocks]
+        old_x, old_y = self.x, self.y
+        
+        # Поворачиваем матрицу фигуры
         self.type = np.rot90(self.type)
         self.blocks.clear()
+        self.create_figure_shape()
+        
+        # Пытаемся скорректировать позицию, если поворот невозможен
+        adjustments = [0, -1, 1, -2, 2]  # Попробуем разные смещения
+        for dx in adjustments:
+            for dy in adjustments:
+
+                if (self.check_collisions(dx * sts.cell_size, dy * sts.step_down, inactive_blocks)
+                    and self.check_sides(surface, dx * sts.cell_size) 
+                    and self.check_bottom(surface, dy * sts.step_down) 
+                ):
+                    # Если нашли подходящее смещение - применяем его
+                    for block in self.blocks:
+                        block.move(dx * sts.cell_size, 0)
+                    self.x += dx * sts.cell_size
+                    return
+            
+        # Если не удалось найти подходящую позицию - отменяем поворот
+        self.type = old_type
+        self.blocks = old_blocks
+        self.x, self.y = old_x, old_y
         self.create_figure_shape()
 
     def draw(self, surface):
--- tetris.py
+++ tetris.py
@@ -12,7 +12,7 @@ class Tetris:
         self.pz_screen = self.screen.subsurface((*sts.pz_pos, sts.pz_width, sts.pz_height))
         pygame.display.set_caption("Тетрис Демо")
         self.active_figure = None
-        self.blocks = []
+        self.inactive_blocks = []
         self.run_game = True
         self.clock = pygame.time.Clock()
     
@@ -26,11 +26,7 @@ class Tetris:
         elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
             self.active_figure.moving_down = True
         elif event.key == pygame.K_SPACE:
-            self.active_figure.rotate()
-            # Сдвигаем фигуру до тех пор пока она не влезет в игровое поле в перевернутом виде
-            while not self.active_figure.check_sides(self.pz_screen, 0):
-                for block in self.active_figure.blocks:
-                    block.move(-sts.step_side, 0)
+            self.active_figure.rotate(self.pz_screen, self.inactive_blocks)
 
     def key_up_events(self, event):
         if event.key == pygame.K_LEFT or event.key == pygame.K_a:
@@ -64,10 +60,10 @@ class Tetris:
     def add_inactive_blocks(self, blocks):
         for block in blocks[:]:
             block.color = sts.inactive_color
-            self.blocks.append(block)
+            self.inactive_blocks.append(block)
 
     def draw_inactive_block(self):
-        for block in self.blocks:
+        for block in self.inactive_blocks:
             block.draw(self.pz_screen)
 
     def start_game(self):
@@ -78,7 +74,10 @@ class Tetris:
                 self.add_inactive_blocks(self.active_figure.blocks)
                 self.create_figure(self.pz_screen)
             self.handle_events()
-            self.active_figure.move(surface=self.pz_screen)
+            self.active_figure.move(
+                surface=self.pz_screen, 
+                inactive_blocks=self.inactive_blocks
+            )
             self.update_screen()
             self.clock.tick(sts.framerate)
             
