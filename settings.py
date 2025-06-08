9a77c4ec2d5a69be581ac280d5bb5253f0c3a616
AlexLoner <lukyanov9567@gmail.com>
2025-06-08 17:41:31 +0300

    adjust game field and speed

--- settings.py
+++ settings.py
@@ -2,24 +2,24 @@ import numpy as np
 
 
 # Screen 
-screen_width = 1000
-screen_height = 700
+screen_width = 800
+screen_height = 1000
 bg_color = (66, 158, 245)
 
 # Play Zone
-pz_width = 400
-pz_height = 600
+pz_width = 40 * 12
+pz_height = 800
 pz_pos = ((screen_width - pz_width) // 2, (screen_height - pz_height) // 2)
 pz_bg_color = (255, 255, 255)
 
 # Block
-cell_size = 20
-step_down = 10
+cell_size = 40
+step_down = 5
 step_side = 20
 
 # Game
 speed_coef = 2
-framerate = 5
+framerate = 10
 
 # Colors
 inactive_color = (169, 169, 169)
